56bc9638c3b48bfe3a83fb972286275894d7175b
Anthony Lucas Floreano <165095578+AnthonyLucasF@users.noreply.github.com>
2025-06-14 23:09:41 -0500

    Borrar src para cambio completo

--- src/routes/tipo.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getTipo } from '../controladores/tipoCtrl.js'
-
-const router = Router()
-
-//Armar nuestras rutas
-router.get('/tipo', getTipo) //SELECT
-
-export default router
